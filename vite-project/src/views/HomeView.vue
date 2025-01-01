8b6f2d5396b5ecff21f881ee8ff90dab6b3d19a8
Jay Chen <chenjie@chenjie.info>
2025-01-01 23:58:50 +0800

    todolist

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import TodoList from '../components/TodoList.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
   </main>
+  <TodoList />
 </template>
