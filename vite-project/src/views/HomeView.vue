be694054a9a7ae418a953200eed4348cdd3a8119
Jay Chen <chenjie@chenjie.info>
2025-01-04 15:12:53 +0800

    rate

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
-import TodoList from '../components/TodoList.vue'
+import TodoList from '../components/Todolist.vue'
+
+import { ref } from 'vue'
+import Rate from '../components/Rate.vue'
+const score = ref(3.5)
 </script>
 
 <template>
@@ -8,4 +12,7 @@ import TodoList from '../components/TodoList.vue'
     <TheWelcome />
   </main>
   <TodoList />
+  <Rate v-model="score">课程评分:</Rate>
+  <Rate v-model="score" theme="green">课程评分:</Rate>
+  <Rate v-model="score">    <img width="14" src="/favicon.ico"></Rate>
 </template>
