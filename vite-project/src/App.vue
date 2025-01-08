3e1ae90c20c1cd22734113295feaf81da1c9791c
Jay Chen <chenjie@chenjie.info>
2025-01-08 12:24:36 +0800

    npm install element3 --save

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,86 +1,67 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="hello world !" />
+  <el-container>
+    <el-header>
+      <el-menu :default-active="1" class="el-menu-demo" mode="horizontal" background-color="#545c64" text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="2">
+          <template v-slot:title>我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template v-slot:title>选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="3" disabled>消息中心</el-menu-item>
+        <el-menu-item index="4"><a href="https://element3-ui.com" target="_blank">订单管理</a></el-menu-item>
+      </el-menu>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/count">Count</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <div>
+          <router-link to="/"> Home</router-link>
+        </div>
+        <div>
+          <router-link to="/about">About</router-link>
+        </div>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+</script>
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
   text-align: center;
-  margin-top: 2rem;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+body>.el-container {
+  margin-bottom: 40px;
 }
 </style>
--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -4,9 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element3 from 'element3'
+import 'element3/lib/theme-chalk/index.css'
 
 const app = createApp(App)
 app.use(store)
-app.use(router)
+.use(router)
+.use(Element3)
 
 app.mount('#app')
+
+
